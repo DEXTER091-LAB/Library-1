b3a279cef5a4bd60f2f9c3e234c340cf1220f41b
Dichan Shrestha <blackuniverse.808@gmail.com>
2024-02-15 13:59:54 +0545

    cookie added

--- addBook.php
+++ addBook.php
@@ -1,6 +1,29 @@
 <?php
-session_start();
 include('./logic.php');
+
+// Check if the form is submitted
+if (isset($_POST['submit'])) {
+    $book = $_POST['book'];
+    $author = $_POST['author'];
+    $genre = $_POST['genre'];
+    $quantity = $_POST['quantity'];
+    $id = getBookIdByTitle($book);
+    // Set a cookie for the book title
+    setcookie('book_id', $id, time() + (86400 * 30), "/"); // Cookie lasts for 30 days
+    
+    // Attempt to add the book
+    $isAdded = addBook($book, $author, $genre, $quantity);
+    if ($isAdded) {
+        echo 'added';
+    } else {
+        echo 'failed';
+    }
+}
+
+// Accessing the cookie after setting it
+if (isset($_COOKIE['book_id'])) {
+    echo $_COOKIE['book_id'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,11 +31,10 @@ include('./logic.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add Book</title>
 </head>
 
 <body>
-    <!-- echo(addBook('book1', 'author1', 'genre1', 10)); -->
     <form method="post">
         <label for="book">Enter book name: </label>
         <input type="text" name="book" id="book"><br>
@@ -24,22 +46,6 @@ include('./logic.php');
         <input type="text" name="quantity" id="quantity"><br>
         <input type="submit" name="submit" value="Add">
     </form>
-    <?php
-    if (isset($_POST['submit'])) {
-        $book = $_POST['book'];
-        $author = $_POST['author'];
-        $genre = $_POST['genre'];
-        $quantity = $_POST['quantity'];
-        $id = getBookIdByTitle($book);
-        $_SESSION['title'] = $id;
-        $isAdded = addBook($book, $author, $genre, $quantity);
-        if($isAdded) {
-            echo 'added';
-        } else {
-            echo 'failed';
-        }
-    }
-    ?>
 </body>
 
 </html>
--- feedBack.php
+++ feedBack.php
@@ -1,10 +1,42 @@
+<?php
+include './logic.php';
+
+// Check if the form is submitted
+if (isset($_POST['submit'])) {
+    $rating = $_POST['rating'];
+    $comment = $_POST['comment'];
+
+    //Check if the 'book_title' cookie is set
+    if (isset($_COOKIE['book_id'])) {
+        $bookId = $_COOKIE['book_id'];
+        
+        // Check if the 'email' cookie is set
+        if (isset($_COOKIE['email'])) {
+            $email = $_COOKIE['email'];
+            $id = getUserIdByEmail($email);
+
+            // Provide feedback
+            $isFeedbacked = provideFeedback($id, $bookId, $rating, $comment);
+            if ($isFeedbacked) {
+                echo 'done feedback';
+            } else {
+                echo 'not done';
+            }
+        } else {
+            echo "Email is not set in cookies.";
+        }
+    } else {
+        echo "Book id is not set in cookies.";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Provide Feedback</title>
     <!-- Include Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
 
@@ -16,31 +48,6 @@
         <input type="text" placeholder="comment" name="comment">
         <input type="submit" name="submit" value="submit">
     </form>
-    <?php
-    session_start();
-    include './signUp.php';
-    include './addBook.php';
-    include './logic.php';
-    if (isset($_SESSION['email'])) {
-        $id = getUserIdByEmail($email);
-        if (isset($_POST['submit'])) {
-            $rating = $_POST['rating'];
-            $comment = $_POST['comment'];
-            if (isset($_POST['title'])) {
-                $bookId = isset($_POST['title']);
-                $isFeedbacked = provideFeedback($id, $bookId, $rating, $comment);
-                if($isFeedbacked) {
-                    echo 'done feedback';
-                } else {
-                    echo "not done";
-                }
-            }
-        }
-        // provideFeedback($userId, $bookId, $rating, $comment);
-    } else {
-        echo "Email is not set in session.";
-    }
-    ?>
 </body>
 
 </html>
